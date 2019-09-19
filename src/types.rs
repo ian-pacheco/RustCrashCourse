8308c3b7b7185786896baecb2cc791d6c7d190d2
Ian Pacheco <ianpacheco64@gmail.com>
2019-09-19 00:44:44 -0300

    Intro - Cargo Init & Build, Print & Formatting, Variables and Data Types

---
+++ src/types.rs
@@ -0,0 +1,14 @@
+/*
+Primitive types--
+(number od bits they take in memory)
+Intergers: u8, i8, u16, i16, u32, i32, u64, i64, u128, i128 
+Floats: f32, f64
+Boolean: bool
+characters: char
+Tuples
+Arrays
+*/
+
+pub fn run() {
+    
+}
