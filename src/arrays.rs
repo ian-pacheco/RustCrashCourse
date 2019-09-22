2d1a70dbd4906d09bc9847eb7014e86c71acec7c
Ian Pacheco <ianpacheco64@gmail.com>
2019-09-22 01:25:15 -0300

    arrays, tuples and vectors

---
+++ src/arrays.rs
@@ -0,0 +1,26 @@
+// Arrays - Fixed list where elemnets are the same data types
+
+use std::mem;
+
+pub fn run() {
+    let mut number: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // Re assing value
+    number[2] = 20;
+
+    println!("{:?}", number);
+
+    // Get single value
+    println!("Single value {}", number[0]);
+
+    // Get array length
+    println!("Array length: {}", number.len());
+
+    // Arrays are stack allocated
+    println!("Arrays occupaies {} bytes", mem::size_of_val(&number));
+
+    // Get Slice
+    let slice: &[i32] = &number[0..2];
+
+    println!("Slice {:?}", slice);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 // mod print;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+// mod tuples;
+// mod arrays;
+mod vectors;
 
 fn main() {
     // print::run();
     // vars::run();
     // types::run();
-    strings::run();
+    // strings::run();
+    // tuples::run();
+    // arrays::run();
+    vectors::run();
 }
