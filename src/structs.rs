f999a44913865faa00f47ef429377eb640f977e2
Ian Pacheco <ianpacheco64@gmail.com>
2019-09-25 09:45:08 -0300

    Completo

--- src/structs.rs
+++ src/structs.rs
@@ -8,7 +8,37 @@
 // }
 
 // Tuple struct
-struct Color(u8,u8,u8);
+// struct Color(u8,u8,u8);
+
+struct Person {
+    first_name: String,
+    last_name: String
+}
+
+impl Person {
+    // Constructor person
+    fn new(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string()
+        }
+    }
+
+    fn full_name(&self) -> String{
+        format!("{} {}", self.first_name, self.last_name)
+    }
+
+    // Set last name
+    fn set_last_name(&mut self, last: &str) {
+        self.last_name = last.to_string();
+    }
+
+    // Name to tuple
+    fn to_tuple(self) -> (String, String) {
+        (self.first_name, self.last_name)
+    }
+}
+
 pub fn run() {
     // let mut c = Color{
     //     red: 255,
@@ -20,5 +50,12 @@ pub fn run() {
 
     // println!("Color: {} {} {}", c.red, c.green, c.blue);
 
-    let mut c = Color(255, 0, 0);
+    // let mut c = Color(255, 0, 0);
+
+    let mut p = Person::new("Mary", "Doe");
+    println!("Person: {}", p.full_name());
+    p.set_last_name("Willians");
+    println!("Person: {}", p.full_name());
+    println!("Person Tuple: {:?}", p.to_tuple());
+
 }
